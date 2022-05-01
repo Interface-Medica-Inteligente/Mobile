59c5063004b44955d4d9ac7d6cc84c94a5d0e420
Phewrys <tiago.farias@dcomp.ufs.br>
2022-04-30 21:53:55 -0300

    feat: Criação do component de botão circular.

---
+++ src/components/ButtonSmall/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components/native"
+
+export const Button = styled.TouchableOpacity`
+  width: 70px;
+  height: 70px;
+  align-self: flex-end;
+  padding: 15px;
+  margin: 20px 0px;
+  background-color: #00CEC8;
+  color: #FFF;
+  border-radius: 73px;
+`
+
+export const Text = styled.Text`
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 35px;
+  text-align: center;
+  color: #FFFFFF;
+`
