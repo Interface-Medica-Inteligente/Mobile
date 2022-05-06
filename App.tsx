048c3fb0a8ec74e2723073705e379dd17b5f079d
Isaac Santos de Oliveira <42910855+isaac-oliveira@users.noreply.github.com>
2022-05-06 17:59:13 -0300

    fix: adiciona o provider do redux

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import "react-native-gesture-handler";
 import React, { useState, useEffect } from "react";
+import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import MainStack, { RootStackParamList } from "./src/stacks/MainStack";
 import { Platform, UIManager } from "react-native";
+import store from './store'
 
 if (Platform.OS === "android") {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
@@ -12,8 +14,10 @@ if (Platform.OS === "android") {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <MainStack />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <MainStack />
+      </NavigationContainer>
+    </Provider>
   );
 }
